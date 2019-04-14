e9cb2645a621549f8e5491fd0b786020f760f648
menakafdo <48564572+menakafdo@users.noreply.github.com>
2019-04-14 22:00:12 +0100

    Add files via upload

--- aircraft_loading.m
+++ aircraft_loading.m
@@ -7,11 +7,15 @@ N_comp = 16;
 hold = 4; 
 % a stability constraint provided by the aircraft manifacture 
 %eps = 
-Ma = 180000;    % mass of the aircraft
-Mmax = 275000;
-Mpax = 30000;
-Mmail= 5000;
-Mc = Mmax-Ma-Mpax-Mmail;
+Ma = 180000;    % zero fuel weight of the aircraft
+Mmax = 275000; %Maximum load aircraft can carry
+Mpax = 22125;  %Max passenger weight, max passengers= 295 and ~ 75kg of one passsenger weight
+Mfuel= 95000; %3.23 kg per 4.72L when the fuel used is aviation gas (total fuel cap = 140640L) 
+Mmail = 5000;   %Max mail weight
+Mbulk = 35000;    %bulk cargo
+Mlandweight=192000;
+
+Mc = Mmax-Mpax-Mfuel-Mmail-Mbulk;
 Mi = zeros(N_cont,1);
 
 X_a = 15.1;            %position of the CG before loading
@@ -50,8 +54,6 @@ MM= [Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi];
  Mx = sum(sum(MM.*x)); % Main Objective
  Mxx = (x.*MM)*X_j;
  airload.Objective = Mx;
-
-
  noDuplicates = sum(x,1)<=1;
  noDuplicatesPos = sum(x,2)<=1;
  airload.Constraints.noDuplicates = noDuplicates; 
@@ -60,26 +62,14 @@ MM= [Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi,Mi];
  maxWeight = Mx<=Mc;     % Stress conditions
  
  airload.Constraints.maxWeight = maxWeight;
- 
- %CGx = Ma*X_a + (Mxx'*X_j)./(Ma + Mmax);
-
- %CGCons = CGx <= X_stable;
- %airload.Constraints.CGCons = CGCons;
-
- 
- %stability constaints 
- 
-  stability1 = (Ma + Mx)*(X_target - eps) <= Ma*X_a +sum(sum(Mxx));
-  stability2 =  Ma*X_a + sum(sum(Mxx)) <= (Ma + Mx)*(X_target + eps); 
+  %CGx = Ma*X_a + (Mxx'*X_j)./(Ma + Mmax); %CGCons = CGx <= X_stable; %airload.Constraints.CGCons = CGCons; %stability constaints 
+ stability1 = (Ma + Mx)*(X_target - eps) <= Ma*X_a +sum(sum(Mxx));
+ stability2 =  Ma*X_a + sum(sum(Mxx)) <= (Ma + Mx)*(X_target + eps); 
   
  airload.Constraints.stability1 = stability1; 
  airload.Constraints.stability2 = stability2;
- 
  options = optimoptions(airload);
  options.MaxNodes= 500000;
- 
-
-
  [sol,fval] = solve(airload, 'Options', options);
 %%
 
